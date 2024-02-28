415efb7d7015ed6ba5aa70d1e019f7bd7b7bbac5
agunbuhori <agun.buhori@gmail.com>
2024-02-28 08:08:16 +0000

    highlight moved to each pages

--- lib/src/features/tilawah/components/pressable_image.dart
+++ lib/src/features/tilawah/components/pressable_image.dart
@@ -1,11 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:quran/src/common/consts/ayah_info_database.dart';
+import 'package:quran/src/config/sqlite.dart';
+import 'package:quran/src/features/tilawah/components/ayah_highlighter.dart';
+import 'package:sqflite/sqflite.dart';
 
-class PressableImage extends StatelessWidget {
+class PressableImage extends StatefulWidget {
   final String imageAssetPath;
-  final Function(double x, double y) onTap;
-
+  final int page;
   const PressableImage(
-      {super.key, required this.imageAssetPath, required this.onTap});
+      {super.key, required this.imageAssetPath, required this.page});
+
+  @override
+  State<PressableImage> createState() => _PressableImage();
+}
+
+class _PressableImage extends State<PressableImage> {
+  late Widget higlights = const SizedBox();
+  // final Function(double x, double y) onTap;
+
+  List<double> convertToScreenCoordinate(
+      int minX, int minY, int maxX, int maxY) {
+    double imageWidth = 1024;
+    double imageHeight = 1657;
+    double screenWidth = 393;
+    double screenHeight = 636.88;
+
+    // Calculate scaling factors
+    double scaleX = screenWidth / imageWidth;
+    double scaleY = screenHeight / imageHeight;
+
+    // Scale coordinates
+    double scaledMinX = minX * scaleX;
+    double scaledMinY = minY * scaleY;
+    double scaledMaxX = maxX * scaleX;
+    double scaledMaxY = maxY * scaleY;
+
+    return [scaledMinX, scaledMinY, scaledMaxX, scaledMaxY];
+  }
+
+  Future<void> searchTheSameGlyphs(int suraNumber, int ayahNumber) async {
+    Database database = await SQLite.getDatabase(AyahInfoDatabase.dbName);
+    List<Map<String, dynamic>> glyphsQuery = await database.query('glyphs',
+        where: "sura_number = ? AND ayah_number = ?",
+        whereArgs: [suraNumber, ayahNumber]);
+
+    if (glyphsQuery.isNotEmpty) {
+      List<Widget> temps = [];
+
+      for (var g in glyphsQuery) {
+        List<double> scaled = convertToScreenCoordinate(
+            g['min_x'], g['min_y'], g['max_x'], g['max_y']);
+
+        temps.add(Positioned(
+          child: CustomPaint(
+            painter: AyahHighlighter(
+                minX: scaled[0],
+                minY: scaled[1],
+                maxX: scaled[2],
+                maxY: scaled[3]),
+          ),
+        ));
+
+        setState(() {
+          higlights = Stack(children: temps);
+        });
+      }
+    }
+  }
+
+  Future<void> searchGlyphs(double x, double y) async {
+    Database database = await SQLite.getDatabase(AyahInfoDatabase.dbName);
+    List<Map<String, dynamic>> glyphsQuery = await database.rawQuery('''
+      SELECT * FROM glyphs
+      WHERE page_number = ?
+      AND min_x <= ?
+      AND min_y <= ?
+      AND max_x >= ?
+      AND max_y >= ?
+    ''', [widget.page, x, y, x, y]);
+
+    if (glyphsQuery.isNotEmpty) {
+      searchTheSameGlyphs(
+          glyphsQuery.first['sura_number'], glyphsQuery.first['ayah_number']);
+    }
+  }
 
   // Function to transform coordinates based on the new MaxX
   List<double> transformCoordinates(double oldX, double oldY, double oldMaxX,
@@ -27,36 +105,46 @@ class PressableImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTapDown: (details) {
-        Image image = Image.asset(imageAssetPath);
-        image.image.resolve(ImageConfiguration.empty).addListener(
-          ImageStreamListener((ImageInfo info, bool _) {
-            double imageWidth = info.image.width.toDouble();
-            double imageHeight = info.image.height.toDouble();
-            double screenWidth = MediaQuery.of(context).size.width;
+    return Center(
+      child: Stack(
+        children: [
+          GestureDetector(
+            onTapDown: (details) {
+              Image image = Image.asset(widget.imageAssetPath);
+              image.image.resolve(ImageConfiguration.empty).addListener(
+                ImageStreamListener((ImageInfo info, bool _) {
+                  double imageWidth = info.image.width.toDouble();
+                  double imageHeight = info.image.height.toDouble();
+                  double screenWidth = MediaQuery.of(context).size.width;
 
-            double x = (details.localPosition.dx / imageWidth) * imageWidth;
-            double y = (details.localPosition.dy / imageHeight) * imageHeight;
+                  double x =
+                      (details.localPosition.dx / imageWidth) * imageWidth;
+                  double y =
+                      (details.localPosition.dy / imageHeight) * imageHeight;
 
-            double resizedImageHeight = getCalculatedImageHeightByRatio(
-                imageWidth, imageHeight, screenWidth);
+                  double resizedImageHeight = getCalculatedImageHeightByRatio(
+                      imageWidth, imageHeight, screenWidth);
 
-            List<double> transformed = transformCoordinates(
-                x, y, screenWidth, resizedImageHeight, 1024);
-            // why 1120?
-            // because on database refer to 1120 width image
-
-            onTap(transformed.elementAt(0), transformed.elementAt(1));
-          }),
-        );
-      },
-      child: Image.asset(
-        imageAssetPath,
-        width: MediaQuery.of(context)
-            .size
-            .width, // Lebar gambar mengikuti lebar layar
-        fit: BoxFit.fill, // Mengisi gambar ke dalam kontainer
+                  List<double> transformed = transformCoordinates(
+                      x, y, screenWidth, resizedImageHeight, 1024);
+                  // why 1120?
+                  // because on database refer to 1120 width image
+                  searchGlyphs(
+                      transformed.elementAt(0), transformed.elementAt(1));
+                  // onTap(transformed.elementAt(0), transformed.elementAt(1));
+                }),
+              );
+            },
+            child: Image.asset(
+              widget.imageAssetPath,
+              width: MediaQuery.of(context)
+                  .size
+                  .width, // Lebar gambar mengikuti lebar layar
+              fit: BoxFit.fill, // Mengisi gambar ke dalam kontainer
+            ),
+          ),
+          higlights
+        ],
       ),
     );
   }
--- lib/src/features/tilawah/tilawah_page.dart
+++ lib/src/features/tilawah/tilawah_page.dart
@@ -19,74 +19,6 @@ class TilawahPage extends StatefulWidget {
 class _TilawahPageState extends State<TilawahPage> {
   late PageController pageController =
       PageController(initialPage: widget.surah.pageStart - 1);
-  late Widget higlights = const Stack();
-
-  List<double> convertToScreenCoordinate(
-      int minX, int minY, int maxX, int maxY) {
-    double imageWidth = 1024;
-    double imageHeight = 1657;
-    double screenWidth = 393;
-    double screenHeight = 636.88;
-
-    // Calculate scaling factors
-    double scaleX = screenWidth / imageWidth;
-    double scaleY = screenHeight / imageHeight;
-
-    // Scale coordinates
-    double scaledMinX = minX * scaleX;
-    double scaledMinY = minY * scaleY;
-    double scaledMaxX = maxX * scaleX;
-    double scaledMaxY = maxY * scaleY;
-
-    return [scaledMinX, scaledMinY, scaledMaxX, scaledMaxY];
-  }
-
-  Future<void> searchTheSameGlyphs(int suraNumber, int ayahNumber) async {
-    Database database = await SQLite.getDatabase(AyahInfoDatabase.dbName);
-    List<Map<String, dynamic>> glyphsQuery = await database.query('glyphs',
-        where: "sura_number = ? AND ayah_number = ?",
-        whereArgs: [suraNumber, ayahNumber]);
-
-    if (glyphsQuery.isNotEmpty) {
-      List<Widget> temps = [];
-
-      for (var g in glyphsQuery) {
-        List<double> scaled = convertToScreenCoordinate(
-            g['min_x'], g['min_y'], g['max_x'], g['max_y']);
-
-        temps.add(Positioned(
-          child: CustomPaint(
-            painter: AyahHighlighter(
-                minX: scaled[0],
-                minY: scaled[1],
-                maxX: scaled[2],
-                maxY: scaled[3]),
-          ),
-        ));
-
-        setState(() {
-          higlights = Stack(children: temps);
-        });
-      }
-    }
-  }
-
-  Future<void> searchGlyphs(double x, double y, int page) async {
-    Database database = await SQLite.getDatabase(AyahInfoDatabase.dbName);
-    List<Map<String, dynamic>> glyphsQuery = await database.rawQuery('''
-      SELECT * FROM glyphs
-      WHERE page_number = ?
-      AND min_x <= ?
-      AND min_y <= ?
-      AND max_x >= ?
-      AND max_y >= ?
-    ''', [page, x, y, x, y]);
-
-    if (glyphsQuery.isNotEmpty) {
-      searchTheSameGlyphs(
-          glyphsQuery.first['sura_number'], glyphsQuery.first['ayah_number']);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -96,18 +28,9 @@ class _TilawahPageState extends State<TilawahPage> {
             controller: pageController,
             reverse: true,
             itemBuilder: (BuildContext context, index) {
-              return Center(
-                child: Stack(
-                  children: [
-                    PressableImage(
-                        imageAssetPath: LocalAssets.getQuranPage(index + 1),
-                        onTap: (x, y) {
-                          searchGlyphs(x, y, index + 1);
-                        }),
-                    Positioned(child: higlights)
-                  ],
-                ),
-              );
+              return PressableImage(
+                  page: index + 1,
+                  imageAssetPath: LocalAssets.getQuranPage(index + 1));
             },
             itemCount: 604));
   }
